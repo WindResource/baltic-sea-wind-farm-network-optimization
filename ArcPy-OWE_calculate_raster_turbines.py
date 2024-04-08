21e443c7ba2fabfd4b3de6db62d9dd4bf6c9a510
Constantijn de Wilt <cfldew@gmail.com>
2024-04-08 15:49:24 +0200

    Calculate water depth, Weibull-A, and Weibull-k values for a point from projected raster layers and add them to the attribute table of an offshore substation feature layer.

--- ArcPy-OWE_calculate_raster_turbines.py
+++ ArcPy-OWE_calculate_raster_turbines.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def calculate_raster() -> None:
     """
-    Calculate water depth from a projected bathymetry raster and add it to the attribute table of an offshore substation feature layer.
+    Calculate water depth, Weibull-A, and Weibull-k values for a point from projected raster layers and add them to the
+    attribute table of an offshore substation feature layer.
 
     Returns:
     - None
@@ -15,21 +16,29 @@ def calculate_raster() -> None:
 
     # Initialize variables
     coord_layer = None
-    raster_layers = []
+    raster_layer_lists = {'bathymetry': [], 'Weibull-A': [], 'Weibull-k': []}
     
     # Find layers
     for layer in map_layer.listLayers():
-        if layer.isFeatureLayer and layer.name.startswith('OSSC'):
+        if layer.isFeatureLayer and layer.name.startswith('WTC'):
             coord_layer = layer
-        elif layer.isRasterLayer and 'bathymetry' in layer.name:
-            raster_layers.append(layer)
+        elif layer.isRasterLayer:
+            for key in raster_layer_lists.keys():
+                if key in layer.name:
+                    raster_layer_lists[key].append(layer)
     
     # Error handling
     if coord_layer is None:
-        arcpy.AddError("No feature layer starting with 'OSSC' found in the current map.")
+        arcpy.AddError("No feature layer starting with 'WTC' found in the current map.")
         return
-    if not raster_layers:
+    if not raster_layer_lists['bathymetry']:
         arcpy.AddError("No raster layers with 'bathymetry' found in the current map.")
+        return
+    if not raster_layer_lists['Weibull-A']:
+        arcpy.AddError("No raster layers with 'Weibull-A' found in the current map.")
+        return
+    if not raster_layer_lists['Weibull-k']:
+        arcpy.AddError("No raster layers with 'Weibull-k' found in the current map.")
         return
 
     # Deselect all currently selected features
@@ -37,25 +46,23 @@ def calculate_raster() -> None:
     
     arcpy.AddMessage(f"Processing layer: {coord_layer.name}")
     
-    # Convert raster layers to paths
-    raster_paths = [raster_layer.dataSource for raster_layer in raster_layers]
-    
-    # Convert rasters to numpy arrays
-    raster_arrays = [arcpy.RasterToNumPyArray(raster_path, nodata_to_value=np.nan) for raster_path in raster_paths]
-    
-    # Get raster properties
-    raster_descs = [arcpy.Describe(raster_layer) for raster_layer in raster_layers]
-    extents = [raster_desc.extent for raster_desc in raster_descs]
-    cell_widths = [raster_desc.meanCellWidth for raster_desc in raster_descs]
-    cell_heights = [raster_desc.meanCellHeight for raster_desc in raster_descs]
+    # Convert raster layers to paths and numpy arrays
+    raster_arrays_lists = {}
+    raster_descs_lists = {}
+    for key in raster_layer_lists.keys():
+        raster_paths = [layer.dataSource for layer in raster_layer_lists[key]]
+        raster_arrays_lists[key] = [arcpy.RasterToNumPyArray(path, nodata_to_value=np.nan) for path in raster_paths]
+        raster_descs_lists[key] = [arcpy.Describe(layer) for layer in raster_layer_lists[key]]
 
-    # Add 'WaterDepth' field if it does not exist in the feature layer
-    field_name = "WaterDepth"
-    if field_name not in [field.name for field in arcpy.ListFields(coord_layer)]:
-        arcpy.management.AddField(coord_layer, field_name, "DOUBLE")
+    # Add fields if they do not exist in the feature layer
+    fields_to_add = ["WaterDepth", "WeibullA", "WeibullK"]
+    existing_fields = [field.name for field in arcpy.ListFields(coord_layer)]
+    for field in fields_to_add:
+        if field not in existing_fields:
+            arcpy.management.AddField(coord_layer, field, "DOUBLE")
 
-    # Update the attribute table with water depth values
-    with arcpy.da.UpdateCursor(coord_layer, ["SHAPE@", field_name]) as cursor:
+    # Update the attribute table with water depth, Weibull-A, and Weibull-k values
+    with arcpy.da.UpdateCursor(coord_layer, ["SHAPE@", "WaterDepth", "WeibullA", "WeibullK"]) as cursor:
         for row in cursor:
             # Get the point geometry
             point_geom = row[0]
@@ -63,30 +70,44 @@ def calculate_raster() -> None:
             # Get the centroid of the point
             point = point_geom.centroid
             
-            water_depth_assigned = False
-            
-            # Iterate over each raster
-            for raster_array, extent, cell_width, cell_height in zip(raster_arrays, extents, cell_widths, cell_heights):
-                # Calculate the column index of the cell containing the point
-                cell_column = round((point.X - extent.XMin) / cell_width)
-                
-                # Calculate the row index of the cell containing the point
-                cell_row = round((extent.YMax - point.Y) / cell_height)
-                
-                # Check if the calculated indices are within bounds
-                if cell_row >= 0 and cell_row < raster_array.shape[0] and cell_column >= 0 and cell_column < raster_array.shape[1]:
-                    # Get the water depth value from the numpy array
-                    water_depth = raster_array[cell_row, cell_column]
-                    
-                    # Update the 'WaterDepth' field in the attribute table
-                    if not np.isnan(water_depth):
-                        row[1] = float(-water_depth)  # Inverse the sign
-                    else:
-                        if row[1] is None:  # If there was no previous value
-                            row[1] = -999  # Assign 0 if water depth cannot be determined
-                    cursor.updateRow(row)
+            # Initialize values
+            water_depth = np.nan
+            weibull_a_value = np.nan
+            weibull_k_value = np.nan
 
-    arcpy.AddMessage("Water depth calculation and attribute update completed.")
+            # Iterate over raster layers
+            for key in raster_arrays_lists.keys():
+                for raster_array, desc in zip(raster_arrays_lists[key], raster_descs_lists[key]):
+                    extent = desc.extent
+                    cell_width = desc.meanCellWidth
+                    cell_height = desc.meanCellHeight
+
+                    # Calculate the column index of the cell containing the point
+                    cell_column = round((point.X - extent.XMin) / cell_width)
+                    
+                    # Calculate the row index of the cell containing the point
+                    cell_row = round((extent.YMax - point.Y) / cell_height)
+                    
+                    # Check if the calculated indices are within bounds
+                    if 0 <= cell_row < raster_array.shape[0] and 0 <= cell_column < raster_array.shape[1]:
+                        # Get the value from the numpy array
+                        value = raster_array[cell_row, cell_column]
+
+                        # Update the corresponding variable based on the key
+                        if key == 'bathymetry':
+                            water_depth = value
+                        elif key == 'Weibull-A':
+                            weibull_a_value = value
+                        elif key == 'Weibull-k':
+                            weibull_k_value = value
+            
+            # Update the attribute table with obtained values
+            row[1] = float(-water_depth) if not np.isnan(water_depth) else -999
+            row[2] = float(weibull_a_value) if not np.isnan(weibull_a_value) else -999
+            row[3] = float(weibull_k_value) if not np.isnan(weibull_k_value) else -999
+            cursor.updateRow(row)
+
+    arcpy.AddMessage("Water depth, Weibull-A, and Weibull-k calculation and attribute update completed.")
 
 # Check if the script is executed standalone or as a module
 if __name__ == "__main__":
