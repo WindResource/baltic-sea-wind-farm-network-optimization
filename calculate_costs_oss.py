16ae7786a12a8485918976878c4790e20f2c7d43
Constantijn de Wilt <cfldew@gmail.com>
2024-05-27 17:53:34 +0200

    Update

--- calculate_costs_oss.py
+++ calculate_costs_oss.py
@@ -1,286 +1,184 @@
-"""
-Calculate installation or decommissioning costs of offshore substations based on the water depth, and port distance.
-
-Parameters:
-- water_depth (float): Water depth at the installation site, in meters.
-- port_distance (float): Distance from the installation site to the nearest port, in kilometers.
-- oss_capacity (float): Capacity of the offshore substation, in units.
-- HVC_type (str, optional): Type of high-voltage converter ('AC' or 'DC'). Defaults to 'AC'.
-- operation (str, optional): Type of operation ('inst' for installation or 'deco' for decommissioning). Defaults to 'inst'.
-
-Returns:
-- float: Calculated installation or decommissioning costs in Euros.
-
-Coefficients:
-- Capacity (u/lift): Capacity of the vessel in units per lift.
-- Speed (km/h): Speed of the vessel in kilometers per hour.
-- Load time (h/lift): Load time per lift in hours per lift.
-- Inst. time (h/u): Installation time per unit in hours per unit.
-- Dayrate (keu/d): Dayrate of the vessel in thousands of euros per day.
-
-Vessels:
-- SUBV (Self-Unloading Bulk Vessels)
-- SPIV (Self-Propelled Installation Vessel)
-- HLCV (Heavy-Lift Cargo Vessels)
-- AHV (Anchor Handling Vessel)
-
-Notes:
-- The function supports both installation and decommissioning operations.
-- Costs are calculated based on predefined coefficients for different support structures and vessels.
-- If the support structure is unrecognized, the function returns None.
-"""
-"""
-Calculate logistics time and costs for major wind turbine repairs (part of OPEX) based on water depth, port distance, and failure rate for major wind turbine repairs.
-
-Coefficients:
-    - Speed (km/h): Speed of the vessel in kilometers per hour.
-    - Repair time (h): Repair time in hours.
-    - Dayrate (keu/d): Dayrate of the vessel in thousands of euros per day.
-    - Roundtrips: Number of roundtrips for the logistics operation.
-
-Returns:
-- tuple: Logistics time in hours per year and logistics costs in Euros.
-"""
-"""
-Add new fields to the attribute table if they do not exist.
-
-Parameters:
-- layer: The layer to which fields will be added.
-- fields_to_add: A list of tuples containing field definitions. Each tuple should contain:
-    - Field name (str): The name of the field.
-    - Field type (str): The data type of the field.
-    - Field label (str): The label or description of the field.
-
-Returns:
-None
-"""
-
 import arcpy
 import numpy as np
 
-def determine_support_structure(water_depth):
+def present_value(equip_cost, inst_cost, ope_cost_yearly, deco_cost):
     """
-    Determines the support structure type based on water depth.
+    Calculate the total present value of cable cost.
+
+    Parameters:
+        equip_cost (float): Equipment cost.
+        inst_cost (float): Installation cost.
+        ope_cost_yearly (float): Yearly operational cost.
+        deco_cost (float): Decommissioning cost.
 
     Returns:
-    - str: Support structure type ('monopile', 'jacket', 'floating', or 'default').
+        float: Total present value of cost.
     """
-    # Define depth ranges for different support structures
-    if water_depth < 30:
-        return "sandisland"
-    elif 30 <= water_depth < 150:
-        return "jacket"
-    elif 150 <= water_depth:
-        return "floating"
+    # Define years for installation, operational, and decommissioning
+    inst_year = 0  # First year
+    ope_year = inst_year + 5
+    dec_year = ope_year + 25  
+    end_year = dec_year + 2  # End year
 
-def calc_equip_costs(water_depth, support_structure, oss_capacity, HVC_type="AC"):
+    # Discount rate
+    discount_rate = 0.05
+
+    # Define the years as a function of inst_year and end_year
+    years = range(inst_year, end_year + 1)
+
+    # Initialize total operational cost
+    ope_cost = 0
+
+    # Adjust cost for each year
+    for year in years:
+        # Adjust installation cost
+        if year == inst_year:
+            equip_cost *= (1 + discount_rate) ** -year
+            inst_cost *= (1 + discount_rate) ** -year
+        # Adjust operational cost
+        if year >= inst_year and year < ope_year:
+            inst_cost *= (1 + discount_rate) ** -year
+        elif year >= ope_year and year < dec_year:
+            ope_cost_yearly *= (1 + discount_rate) ** -year
+            ope_cost += ope_cost_yearly  # Accumulate yearly operational cost
+        # Adjust decommissioning cost
+        if year >= dec_year and year <= end_year:
+            deco_cost *= (1 + discount_rate) ** -year
+
+    # Calculate total present value of cost
+    total_cost = equip_cost + inst_cost + ope_cost + deco_cost
+
+    return total_cost
+
+def eh_cost_lin(water_depth, ice_cover, port_distance, eh_capacity, polarity = "AC"):
     """
-    Calculates the offshore substation equipment costs based on water depth, capacity, and export cable type.
+    Estimate the cost associated with an energy hub based on various parameters.
+
+    Parameters:
+    - water_depth (float): Water depth at the location of the energy hub.
+    - ice_cover (int): Indicator of ice cover presence (1 for presence, 0 for absence).
+    - port_distance (float): Distance from the offshore location to the nearest port.
+    - eh_capacity (float): Capacity of the energy hub.
+    - polarity (str, optional): Polarity of the substation ('AC' or 'DC'). Defaults to 'AC'.
 
     Returns:
-    - float: Calculated equipment costs.
+    - float: Estimated total cost of the energy hub.
     """
-    # Coefficients for equipment cost calculation based on the support structure and year
-    support_structure_coeff = {
-        'sandisland': (3.26, 804, 0, 0),
-        'jacket': (233, 47, 309, 62),
-        'floating': (87, 68, 116, 91)
-    }
+    
+    def supp_struct_cond(water_depth):
+        """
+        Determines the support structure type based on water depth.
 
-    equip_coeff = {
-        'AC': (22.87, 7.06),
-        'DC': (102.93, 31.75)
-    }
-    
-    # Define parameters
-    c1, c2, c3, c4 = support_structure_coeff[support_structure]
-    
-    c5, c6 = equip_coeff[HVC_type]
-    
-    # Define equivalent electrical power
-    equiv_capacity = 0.5 * oss_capacity if HVC_type == "AC" else oss_capacity
+        Returns:
+        - str: Support structure type ('monopile', 'jacket', 'floating', or 'default').
+        """
+        # Define depth ranges for different support structures
+        if water_depth < 150:
+            return "jacket"
+        elif 150 <= water_depth:
+            return "floating"
 
-    if support_structure == 'sandisland':
-        # Calculate foundation costs for sand island
-        area_island = (equiv_capacity * 5)
-        slope = 0.75
-        r_hub = np.sqrt(area_island/np.pi)
-        r_seabed = r_hub + (water_depth + 3) / slope
-        volume_island = (1/3) * slope * np.pi * (r_seabed ** 3 - r_hub ** 3)
+    def equip_cost_lin(water_depth, support_structure, ice_cover, eh_capacity, polarity = "AC"):
+        """
+        Calculates the energy hub equipment cost based on water depth, capacity, and export cable type.
+
+        Returns:
+        - float: Calculated equipment cost.
+        """
+        # Coefficients for equipment cost calculation based on the support structure and year
+        support_structure_coeff = {
+            'jacket': (233, 47, 309, 62),
+            'floating': (87, 68, 116, 91)
+        }
+
+        equip_coeff = {
+            'AC': (22.87, 7.06),
+            'DC': (102.93, 31.75)
+        }
         
-        supp_costs = c1 * volume_island + c2 * area_island
-    else:
-        # Calculate foundation costs for jacket/floating
-        supp_costs = (c1 * water_depth + c2 * 1000) * equiv_capacity + (c3 * water_depth + c4 * 1000)
-    
-    # Power converter costs
-    conv_costs = c5 * oss_capacity * int(1e3) + c6 * int(1e6) #* int(1e3)
-    equip_costs = supp_costs + conv_costs
-    
-    return supp_costs, conv_costs, equip_costs
-
-def calc_costs(water_depth, support_structure, port_distance, oss_capacity, HVC_type = "AC", operation = "inst"):
-    """
-    Calculate installation or decommissioning costs of offshore substations based on the water depth, and port distance.
-
-    Returns:
-    - float: Calculated installation or decommissioning costs.
-    """
-    # Installation coefficients for different vehicles
-    inst_coeff = {
-        ('sandisland','SUBV'): (20000, 25, 2000, 6000, 15),
-        ('jacket' 'PSIV'): (1, 18.5, 24, 96, 200),
-        ('floating','HLCV'): (1, 22.5, 10, 0, 40),
-        ('floating','AHV'): (3, 18.5, 30, 90, 40)
-    }
-
-    # Decommissioning coefficients for different vehicles
-    deco_coeff = {
-        ('sandisland','SUBV'): (20000, 25, 2000, 6000, 15),
-        ('jacket' 'PSIV'): (1, 18.5, 24, 96, 200),
-        ('floating','HLCV'): (1, 22.5, 10, 0, 40),
-        ('floating','AHV'): (3, 18.5, 30, 30, 40)
-    }
-
-    # Choose the appropriate coefficients based on the operation type
-    coeff = inst_coeff if operation == 'inst' else deco_coeff
-
-    if support_structure == 'sandisland':
-        c1, c2, c3, c4, c5 = coeff[('sandisland','SUBV')]
+        # Define parameters
+        c1, c2, c3, c4 = support_structure_coeff[support_structure]
+        
+        c5, c6 = equip_coeff[polarity]
+        
         # Define equivalent electrical power
-        equiv_capacity = 0.5 * oss_capacity if HVC_type == "AC" else oss_capacity
-        
-        # Calculate installation costs for sand island
-        water_depth = max(0, water_depth)
-        area_island = (equiv_capacity * 5)
-        slope = 0.75
-        r_hub = np.sqrt(area_island/np.pi)
-        r_seabed = r_hub + (water_depth + 3) / slope
-        volume_island = (1/3) * slope * np.pi * (r_seabed ** 3 - r_hub ** 3)
-        
-        total_costs = ((volume_island / c1) * ((2 * port_distance) / c2) + (volume_island / c3) + (volume_island / c4)) * (c5 * 1000) / 24
-        
-    elif support_structure == 'jacket':
-        c1, c2, c3, c4, c5 = coeff[('jacket' 'PSIV')]
-        # Calculate installation costs for jacket
-        total_costs = ((1 / c1) * ((2 * port_distance) / c2 + c3) + c4) * (c5 * 1000) / 24
-    elif support_structure == 'floating':
-        total_costs = 0
-        
-        # Iterate over the coefficients for floating (HLCV and AHV)
-        for vessel_type in [('floating', 'HLCV'), ('floating', 'AHV')]:
-            c1, c2, c3, c4, c5 = coeff[vessel_type]
-            
-            # Calculate installation costs for the current vessel type
-            vessel_costs = ((1 / c1) * ((2 * port_distance) / c2 + c3) + c4) * (c5 * 1000) / 24
-            
-            # Add the costs for the current vessel type to the total costs
-            total_costs += vessel_costs
-    else:
-        total_costs = None
-        
-    return total_costs
+        equiv_capacity = 0.5 * eh_capacity if polarity == "AC" else eh_capacity
 
-def update_fields():
-    """
-    Update the attribute table of the Offshore SubStation Coordinates (OSSC) layer.
+        # Calculate foundation cost for jacket/floating
+        supp_cost = (c1 * water_depth + c2 * 1000) * equiv_capacity + (c3 * water_depth + c4 * 1000)
+        
+        # Add support structure cost for ice cover adaptation
+        supp_cost = 1.10 * supp_cost if ice_cover == 1 else supp_cost
+        
+        # Power converter cost
+        conv_cost = c5 * eh_capacity * int(1e3) + c6 * int(1e6) #* int(1e3)
+        
+        # Calculate equipment cost
+        equip_cost = supp_cost + conv_cost
+        
+        return conv_cost, equip_cost
 
-    Returns:
-    - None
-    """
+    def inst_deco_cost_lin(support_structure, port_distance, operation):
+        """
+        Calculate installation or decommissioning cost of offshore substations based on the water depth, and port distance.
+
+        Returns:
+        - float: Calculated installation or decommissioning cost.
+        """
+        # Installation coefficients for different vehicles
+        inst_coeff = {
+            ('jacket' 'PSIV'): (1, 18.5, 24, 96, 200),
+            ('floating','HLCV'): (1, 22.5, 10, 0, 40),
+            ('floating','AHV'): (3, 18.5, 30, 90, 40)
+        }
+
+        # Decommissioning coefficients for different vehicles
+        deco_coeff = {
+            ('jacket' 'PSIV'): (1, 18.5, 24, 96, 200),
+            ('floating','HLCV'): (1, 22.5, 10, 0, 40),
+            ('floating','AHV'): (3, 18.5, 30, 30, 40)
+        }
+
+        # Choose the appropriate coefficients based on the operation type
+        coeff = inst_coeff if operation == 'inst' else deco_coeff
+            
+        if support_structure == 'jacket':
+            c1, c2, c3, c4, c5 = coeff[('jacket' 'PSIV')]
+            # Calculate installation cost for jacket
+            total_cost = ((1 / c1) * ((2 * port_distance) / c2 + c3) + c4) * (c5 * 1000) / 24
+        elif support_structure == 'floating':
+            total_cost = 0
+            
+            # Iterate over the coefficients for floating (HLCV and AHV)
+            for vessel_type in [('floating', 'HLCV'), ('floating', 'AHV')]:
+                c1, c2, c3, c4, c5 = coeff[vessel_type]
+                # Calculate installation cost for the current vessel type
+                vessel_cost = ((1 / c1) * ((2 * port_distance) / c2 + c3) + c4) * (c5 * 1000) / 24
+                # Add the cost for the current vessel type to the total cost
+                total_cost += vessel_cost
+        
+        return total_cost
+
+    # Determine support structure
+    supp_structure = supp_struct_cond(water_depth)
     
-    # Define the capacities for which fields are to be added
-    capacities = [500, 1000, 1500, 2000, 2500]
+    # Calculate equipment cost
+    conv_cost, equip_cost = equip_cost_lin(water_depth, supp_structure, ice_cover, eh_capacity, polarity)
 
-    # Define the expense categories
-    expense_categories = ['Sup', 'Cnv', 'Equ', 'Ins', 'Cap', 'Ope', 'Dec']
+    # Calculate installation and decommissioning cost
+    inst_cost = inst_deco_cost_lin(supp_structure, port_distance, "inst")
+    deco_cost = inst_deco_cost_lin(supp_structure, port_distance, "deco")
 
-    # Define fields to be added if they don't exist
-    fields_to_add = [('SuppStruct', 'TEXT')]
-
-    # Generate field definitions for each capacity and expense category for both AC and DC
-    for capacity in capacities:
-        for category in expense_categories:
-            for sub_type in ['AC', 'DC']:
-                field_name = f'{category}{capacity}_{sub_type}'
-                fields_to_add.append((field_name, 'DOUBLE'))
-
-    # Access the current ArcGIS project
-    aprx = arcpy.mp.ArcGISProject("CURRENT")
-    map = aprx.activeMap
-
-    # Find the offshore substation layer
-    oss_layers = [layer for layer in map.listLayers() if layer.name.startswith('OSSC')]
-
-    # Check if any OSSC layer exists
-    if not oss_layers:
-        arcpy.AddError("No layer starting with 'OSSC' found in the current map.")
-        return
-
-    # Select the first OSSC layer
-    oss_layer = oss_layers[0]
-
-    # Deselect all currently selected features
-    arcpy.SelectLayerByAttribute_management(oss_layer, "CLEAR_SELECTION")
-
-    arcpy.AddMessage(f"Processing layer: {oss_layer.name}")
-
-    # Check if required fields exist in the attribute table
-    fields = arcpy.ListFields(oss_layer)
-    field_names = [field.name for field in fields]
-    required_fields = ['WaterDepth', 'Distance']
-    for field in required_fields:
-        if field not in field_names:
-            arcpy.AddError(f"Required field '{field}' is missing in the attribute table.")
-            return
-
-    # Check if fields to be added already exist
-    fields_not_exist = [field_name for field_name, field_type in fields_to_add if field_name not in field_names]
-
-    # Add new fields to the attribute table if they do not exist
-    if fields_not_exist:
-        arcpy.management.AddFields(oss_layer, fields_to_add)
-
-    # Update each row in the attribute table
-    with arcpy.da.UpdateCursor(oss_layer, field_names) as cursor:
-        for row in cursor:
-            water_depth = row[field_names.index("WaterDepth")]
-            port_distance = row[field_names.index("Distance")]
-
-            # Determine and assign Support structure
-            support_structure = determine_support_structure(water_depth)
-            row[field_names.index('SuppStruct')] = support_structure.capitalize()
-
-            for capacity in capacities:
-                for sub_type in ['AC', 'DC']:
-                    # Round function
-                    def rnd(r):
-                        return round(r / int(1e6), 6)
-
-                    # Material Costs
-                    supp_costs, conv_costs, equip_costs = calc_equip_costs(water_depth, support_structure, capacity, HVC_type=sub_type)
-                    row[field_names.index(f'Sup{capacity}_{sub_type}')] = rnd(supp_costs)
-                    row[field_names.index(f'Cnv{capacity}_{sub_type}')] = rnd(conv_costs)
-                    row[field_names.index(f'Equ{capacity}_{sub_type}')] = rnd(equip_costs)
-
-                    # Installation and Decommissioning Costs
-                    inst_costs = calc_costs(water_depth, support_structure, port_distance, capacity, HVC_type=sub_type, operation="inst")
-                    deco_costs = calc_costs(water_depth, support_structure, port_distance, capacity, HVC_type=sub_type, operation="deco")
-                    row[field_names.index(f'Ins{capacity}_{sub_type}')] = rnd(inst_costs)
-                    row[field_names.index(f'Dec{capacity}_{sub_type}')] = rnd(deco_costs)
-
-                    # Calculate and assign the capital expenses (the sum of the equipment and installation costs)
-                    capital_expenses = equip_costs + inst_costs
-                    row[field_names.index(f'Cap{capacity}_{sub_type}')] = rnd(capital_expenses)
-
-                    # Calculate and assign operating expenses
-                    operating_expenses = 0.03 * conv_costs + 0.015 * supp_costs if support_structure == 'sandisland' else 0.03 * conv_costs
-                    row[field_names.index(f'Ope{capacity}_{sub_type}')] = rnd(operating_expenses)
-
-            cursor.updateRow(row)
-
-    arcpy.AddMessage(f"Attribute table of {oss_layer.name} updated successfully.")
-
+    # Calculate yearly operational cost
+    ope_cost_yearly = 0.03 * conv_cost
+    
+    # Calculate present value of cost    
+    eh_cost = present_value(equip_cost, inst_cost, ope_cost_yearly, deco_cost)
+    
+    # Offshore substation cost in million Euros
+    eh_cost *= 1e-6
+    
+    return eh_cost
 
 if __name__ == "__main__":
     update_fields()
